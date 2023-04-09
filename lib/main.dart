c874c6ab350a2e666c7ba3d8dd911994ef4ed246
Cheperd Yong Yick Chung <cheperdyong65@gmail.com>
2023-04-09 19:32:45 +0800

    commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:adventuregame/story_brain.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(AdventureGame());
+
+class AdventureGame extends StatelessWidget {
+  const AdventureGame({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(), // we are using the dark theme
+      home: StoryPage(),
+    );
+  }
+}
+
+StoryBrain storyBrain = StoryBrain();
+String firstStoryTitle = storyBrain.getStory();
+
+class StoryPage extends StatefulWidget {
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration:
+          BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.jpg"),
+                fit: BoxFit.cover
+            ),
+          ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    storyBrain.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(choiceNumber: 1);
+                    });
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith(
+                            (states) => Colors.black45),
+                    foregroundColor: MaterialStateProperty.resolveWith(
+                            (states) => Colors.white),
+                    shape: MaterialStateProperty.resolveWith(
+                            (states) => BeveledRectangleBorder()),
+                  ),
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        storyBrain.nextStory(choiceNumber: 2);
+                      });
+                      },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                              (states) => Colors.black45),
+                      foregroundColor: MaterialStateProperty.resolveWith(
+                              (states) => Colors.white),
+                      shape: MaterialStateProperty.resolveWith(
+                              (states) => BeveledRectangleBorder()),
+                    ),
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
